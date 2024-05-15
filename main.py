15e7ffa30b1c625da6668ab57da8ed0fd61d9efd
AlexGladkih <167768609+AlexGladkih@users.noreply.github.com>
2024-05-15 21:06:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,145 @@
-# This is a sample Python script.
+# -*- coding: utf-8 -*-
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# блоки кода
+
+x, y = 10, 29
+
+if x < 0:
+    print('Х меньше нуля')
+    z = x ** 2 + y
+else:
+    print('Х больше нуля')
+    z = x - y
+print('Результат', z)
+
+# ср. с С++
+
+# if (x < 0) { printf('Меньше нуля\n'); z = x**2 + y; } else { printf('Больше нуля\n'); z = x - y; } printf('Получается\n', z)
+
+# вложенные блоки кода
+
+name = input('Enter your name >>>')
+if name == 'Ola':
+    opponent = 'Ola'
+else:
+    if name == 'Sofi':
+        opponent = 'Sofi'
+    else:
+        if name == 'Katy':
+            opponent = 'Katy'
+        else:
+            opponent = 'anonymous'
+            print(opponent)
+
+# оператор pass
+
+if num1 < 0:
+    if num2 > 0:
+        result = -num1 + num2
+    else:
+        result = -num1 - num2
+else:
+    result = num1 + num2
+
+# соглашения о стиле кода
+# PEP8 (Python Enhancement Proposal 8) - описан "правильный" стиль программирования в пайтон
+# https://www.python.org/dev/peps/pep-0008/
+
+# 4 пробела на каждый уровень отступа
+
+if x < 0:
+    if y > 0:
+        print('налево!')
+        pass
+    else:
+        print('направо!')
+else:
+    print('стой!')
+
+# Максимальная длина строки
+
+my_poem = ['Варкалось, хливкие шорьки пырялись по наве',
+           'И хрюкотали зелюки как мюмзики в мове',
+           'О бойся Бармаглота, сын! Он так свирлеп и дик',
+           'А в глуше рымит исполин - Злопастный Брандашмыг!', ]
+
+# пробелы в операторах
+
+x = 2
+y = x * x + 1
+is_big = x >= 3000
+
+x = my_poem[-1]
+print(x)
+my_list = [2, 3, 4, 5, 6, ]
+
+# reformat кода
+
+x, y = 3, 8
+
+if x == 3:
+    print(42)
+
+if x < 0:
+    if y > 0:
+        print('налево!')
+    else:
+        print('направо!')
+else:
+    print('стой!')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# названия переменных
+
+x = 34
+if x > 10:
+    print('I need more space for my pets!')
+
+animals = ['cat', 'wolf', 'ostrich']
+if 'lion' in animals:
+    print('Wow!')
+
+animals = ['cat', 'wolf', 'ostrich']
+# но такой стиль используется для названий классов
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# рекомендации PEP8
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# b (одиночная маленькая буква)
+# B (одиночная заглавная буква)
+# но лучше использовать только такие однобуквенные имена
+#   i j k - для циклов
+#   x y z - для координат
+
+# никогда не используйте в названиях переменных одиночные l, I, O  !
+x = 34
+y = 43
+if x > y:
+    print()
+z = 9
+if z > 0:
+    print()
+
+# lowercase (слово в нижнем регистре)
+# lower_case_with_underscores (слова из маленьких букв с подчеркиваниями)
+# UPPERCASE (заглавные буквы)
+# UPPERCASE_WITH_UNDERSCORES (слова из заглавных букв с подчеркиваниями)
+
+# CapitalizedWords (слова с заглавными буквами, или CapWords, или CamelCase).
+#   Замечание: когда вы используете аббревиатуры в таком стиле, пишите все буквы аббревиатуры заглавными —
+#   HTTPServerError лучше, чем HttpServerError.
+
+# mixedCase (отличается от CapitalizedWords тем, что первое слово начинается с маленькой буквы)
+# Capitalized_Words_With_Underscores (слова с заглавными буквами и подчеркиваниями — уродливо!)
+
+
+# автоматическое переименование в PyCharm и подсказки - вам не нужно набирать длинные названия переменных
+
+ss = ['cat', 'wolf', 'ostrich']
+if 'lion' in ss:
+    print('Wow!')
+
+# В каждой уважающей себя компании есть style guide (стайл-гайд) - руководство по стилю написания кода.
+# Практически все они основываются на PEP8, с небольшими исключениями, принятыми в этой команде.
+# Как пример стайл-гайда небольшой компании рекомендую почитать
+# https://github.com/best-doctor/guides/blob/master/guides/python_styleguide.md
